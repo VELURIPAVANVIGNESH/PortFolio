344716f434d67d23ec37fedf56d1cad66340fb01
VELURIPAVANVIGNESH <157129397+VELURIPAVANVIGNESH@users.noreply.github.com>
2025-07-25 09:57:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import bg from './assets/bg.png';
+import Hero from './sections/HeroSection';
+import AboutMeSection from './sections/AboutMeSection';
+import EducationTimeline from './sections/MyEducationSection';
+import SkillsComponent from './sections/MySkills';
+import ProjectsComponent from './sections/Projects';
+import HackathonComponent from './sections/Hackathons';
+import ContactComponent from './sections/ContactMe';
+
+function App() {
+
+
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <AboutMeSection />
+      <EducationTimeline />
+      <SkillsComponent />
+      <ProjectsComponent />
+      <HackathonComponent />
+      <ContactComponent />
+      {/* <img src={bg} alt='Portfolio' className='w-full h-auto' /> */}
+    </>
+  )
+}
+
+export default App
